1b796f96a9db34b18dedd811f348e3a3412a0f54
Daniel Huang <danielhuang@pop-os.localdomain>
2019-06-22 17:17:57 +1000

    SpeedTesterInterface is working

--- SpeedTesterInterface.py
+++ SpeedTesterInterface.py
@@ -5,29 +5,30 @@ import time
 #written by Daniel Huang 
 
 def SendSpeed(speed): #servo and motor interface
-    byteH = speed | 255;
-    byteL = (speed >> 8) | 255
-    checksum = (70+36+steering+speed)%256 #calc 8bit checksum 
-    ser.write((('F${}{}{}').format(chr(byteL),chr(byteH),chr(checksum))).encode('utf-8')) #format into ascii string and then write to port
+    byteL = speed & 0xFF
+    byteH = (speed >> 8) & 0xFF
+    print (byteH,byteL)
+    checksum = (70+36+byteL+byteH)%256 #calc 8bit checksum 
+    ser.write((('F${}{}{}').format(chr(byteH),chr(byteL),chr(checksum))).encode('utf-8')) #format into ascii string and then write to port
     
-#max steering range is 115 to 60
 
 
 ser = serial.Serial('/dev/ttyUSB0') #open serial port 
 
 steering = 115 #sample values 
-speed = 0
+speed = 25500
 steering = 60
 flag = 1
+
 while(True): #makes servo move back and forth and motor start and stop indefinitely 
-    if(speed == 1000):
+    if(speed >= 40500):
         flag = 0
-    elif(speed == 0):
+    elif(speed <= 25500):
         flag = 1
     if(flag == 0):
-        speed+=-1
+        speed+=-10
     elif(flag == 1):
-        speed+=1
+        speed+=10
     print ("speed: ", speed)
     SendSpeed(speed)
 
